b2d18524645cd1bec987dd7210196576b5da5bab
Naman Seth <naman.seth_cs19@gla.ac.in>
2020-08-25 12:43:11 +0530

    add the equals() and hashcode() method in Book.java class

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,43 +6,13 @@
  * */
 package definitions;
 
+import java.util.Objects;
+
 public class Book {
     private String nameOfBook;
     private String nameOfAuthorOfBook;
     private String ISBNNumberOfBook;
     private boolean isBookAvailable;
-
-    public String getNameOfBook() {
-        return nameOfBook;
-    }
-
-    public String getNameOfAuthorOfBook() {
-        return nameOfAuthorOfBook;
-    }
-
-    public String getISBNNumberOfBook() {
-        return ISBNNumberOfBook;
-    }
-
-    public boolean isBookAvailable() {
-        return isBookAvailable;
-    }
-
-    public void setNameOfBook(String nameOfBook) {
-        this.nameOfBook = nameOfBook;
-    }
-
-    public void setNameOfAuthorOfBook(String nameOfAuthorOfBook) {
-        this.nameOfAuthorOfBook = nameOfAuthorOfBook;
-    }
-
-    public void setISBNNumberOfBook(String ISBNNumberOfBook) {
-        this.ISBNNumberOfBook = ISBNNumberOfBook;
-    }
-
-    public void setBookAvailable(boolean bookAvailable) {
-        this.isBookAvailable = bookAvailable;
-    }
 
     public Book(String nameOfBook, String nameOfAuthorOfBook, String ISBNNumberOfBook, boolean isBookAvailable) {
         this.nameOfBook = nameOfBook;
@@ -51,7 +21,56 @@ public class Book {
         this.isBookAvailable = isBookAvailable;
     }
 
+    public String getNameOfBook() {
+        return nameOfBook;
+    }
+
+    public void setNameOfBook(String nameOfBook) {
+        this.nameOfBook = nameOfBook;
+    }
+
+    public String getNameOfAuthorOfBook() {
+        return nameOfAuthorOfBook;
+    }
+
+    public void setNameOfAuthorOfBook(String nameOfAuthorOfBook) {
+        this.nameOfAuthorOfBook = nameOfAuthorOfBook;
+    }
+
+    public String getISBNNumberOfBook() {
+        return ISBNNumberOfBook;
+    }
+
+    public void setISBNNumberOfBook(String ISBNNumberOfBook) {
+        this.ISBNNumberOfBook = ISBNNumberOfBook;
+    }
+
+    public boolean isBookAvailable() {
+        return isBookAvailable;
+    }
+
+    public void setBookAvailable(boolean bookAvailable) {
+        this.isBookAvailable = bookAvailable;
+    }
+
+    @Override
     public String toString() {
         return "Name of the book: " + getNameOfBook() + "Name of the author of the book: " + getNameOfAuthorOfBook() + "ISBn Number of the code: " + getISBNNumberOfBook() + "Is book available? " + isBookAvailable();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return isBookAvailable() == book.isBookAvailable() &&
+                Objects.equals(getNameOfBook(), book.getNameOfBook()) &&
+                Objects.equals(getNameOfAuthorOfBook(), book.getNameOfAuthorOfBook()) &&
+                Objects.equals(getISBNNumberOfBook(), book.getISBNNumberOfBook());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNameOfBook(), getNameOfAuthorOfBook(), getISBNNumberOfBook(), isBookAvailable());
     }
 }
