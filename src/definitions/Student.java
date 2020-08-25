1f9deefb35927dbb7cee370feb3e484a21fa15c3
Naman Seth <naman.seth_cs19@gla.ac.in>
2020-08-25 13:18:48 +0530

    edit author information in Student.java class

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -1,5 +1,5 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
+ *  User: Naman Seth (namanseth01)
  *  Date: 21/08/20
  *  Time: 3:49 PM
  *  File Name : Student.java
@@ -7,4 +7,5 @@
 package definitions;
 
 public class Student {
+
 }
